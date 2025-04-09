21692cd8678412fc3d53f537fe53b71849cfdf3b
jagadeesh4308 <84701569+jagadeesh4308@users.noreply.github.com>
2025-04-09 11:06:20 +0000

    Bubble sort added

--- Sorting Algorithms/sorting_algorithms.py
+++ Sorting Algorithms/sorting_algorithms.py
@@ -1,6 +1,8 @@
-# selects the minimum element in the remaining element and the minimum element will get the starting indexes
-# uses swap technique to swap minimum and the current index value
-
+# selection sort algorithm
+# finds the minimum element in the unsorted list and swaps it with the first element
+# this process is repeated until the list is sorted
+# the time complexity is O(n^2)
+# the space complexity is O(1)
 def selection_sort(list):
     length = len(list)
     for i in range(length-1):
@@ -13,9 +15,23 @@ def selection_sort(list):
         list[i], list[minIdx] = list[minIdx], list[i]
     return list
 
+# Bubble sort algorithm
+#bubbles up the largest element to the end of the list
+# uses swap technique to swap the current index value and the next index value
+# this process is repeated until the list is sorted
+# the time complexity is O(n^2)
+# the space complexity is O(1)
 def bubble_sort(list):
     length = len(list)
-    
+    for i in range(length-1):
+        for j in range(0, length-i-1):
+            if (list[j] > list[j+1]):
+                # swap the elements
+                list[j], list[j+1] = list[j+1], list[j]
+    return list
 
-print(selection_sort([5,21,6,9,3,1]))
-print(bubble_sort([5,21,6,9,3,1]))
+def main():
+    print('selection:', selection_sort([5,21,6,9,3,1]))
+    print('bubble:', bubble_sort([5,21,6,9,3,1]))
+
+main()
